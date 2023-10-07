b915526e89d86ad36bbbcb94b485ebf50c3183f8
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-10-07 15:38:54 +0300

    first commit unit testing description_of_installation

---
+++ src/gamefunction.js
@@ -0,0 +1,11 @@
+export default function healthLevel(object) {
+    if (object.health > 50) {
+      return 'healthy';
+    } if (object.health > 15) {
+      return 'wounded';
+    }
+    return 'critical';
+  }
+  healthLevel({ health: 90, name: 'Маг' });
+  
+ 
